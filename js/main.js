f53595273e0b8968216ff7c8252839acc703dfc1
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2017-10-20 17:05:03 +0900

    main.jsの更新

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ var ms = ms || {};
 
 ms.SCREEN_W = 320;
 ms.SCREEN_H = 480;
+ms.ASSETS = {
+  image: {
+    // Player
+    poniko: './assets/img/poniko.png',
+    // Enemy
+    banaa: './assets/img/banaa.png',
+    dragon: './assets/img/dragon.png',
+    slimusi: './assets/img/slimusi.png',
+    // Object
+    field: './assets/img/field.png',
+    arrow: './assets/img/arrow.png'
+  }
+}
 ms.SCENES = [
   {
     className: 'ms.MainScene',
